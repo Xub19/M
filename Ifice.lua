cea53a7cf5c6f4e27ea8359bfa1ac1c1183fa1c0
Cao Tien Thanh <168630483+XnobYzz@users.noreply.github.com>
2024-11-22 20:15:18 +0700

    Update Ifice.lua

--- Ifice.lua
+++ Ifice.lua
@@ -1,3 +1,9 @@
+local VirtualUser = game:service('VirtualUser')
+game:service('Players').LocalPlayer.Idled:connect(function()
+    VirtualUser:CaptureController()
+    VirtualUser:ClickButton2(Vector2.new())
+end)
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({ 
     Name = "Ijul Piece 2",
