529f479392b7520dea47a579642e7e625f0d6acf
Cao Tien Thanh <168630483+XnobYzz@users.noreply.github.com>
2024-11-23 23:02:59 +0700

    Create ijulpiece2.lua

---
+++ ijulpiece2.lua
@@ -0,0 +1,5 @@
+if game.PlaceId = 18688206652 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Xub19/xtl/refs/heads/main/Ifice.lua"))() 
+else
+  game.Players.LocalPlayer:Kick("Error : Game Not Supported")
+end
