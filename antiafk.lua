ddce5f7b6978543e58329dce9c93cf9abd3e2694
Cao Tien Thanh <168630483+XnobYzz@users.noreply.github.com>
2024-11-27 12:02:15 +0700

    Update antiafk.lua

--- antiafk.lua
+++ antiafk.lua
@@ -1,10 +1,13 @@
-game:GetService("Players").LocalPlayer.Idled:Connect(function()
-	game:GetService("VirtualUser"):CaptureController()
-	game:GetService("VirtualUser"):ClickButton2(Vector2.new())
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = "";
-		Text = "by Xub19";
-		Icon = "rbxthumb://type=Asset&id=77349212873128&w=150&h=150";
-		Duration = 5
+pcall(function()
+	local vu=game:GetService("VirtualUser")
+	game:GetService("Players").LocalPlayer.Idled:Connect(function()
+		vu:CaptureController()
+		vu:ClickButton2(Vector2.new())
+	end)
+	game:GetService("StarterGui"):SetCore("SendNotification",{
+		Title="Anti AFK by Xub19";
+		Text="Status: Active";
+		Icon="rbxthumb://type=Asset&id=77349212873128&w=150&h=150";
+		Duration=10
 	})
 end)
